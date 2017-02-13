d2ab61587931ef1821fc530755b2deeec3918226
Gaurav Vaidya <gaurav@ggvaidya.com>
2017-02-13 14:00:42 -0700

    Modified to save stdout/stderr for debugging.

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 git reset --hard
 git pull origin master
-perl download_as_json.pl tumblr.ggvaidya.com
+perl download_as_json.pl tumblr.ggvaidya.com > log.txt 2> stderr.txt
 git add .
 git commit -a -v -m "Updated to latest"
 git push origin master
